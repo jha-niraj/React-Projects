8adb368f65870c129cef0040f1de58ff97805fce
jha-niraj <jhaniraj45@gmail.com>
2024-06-22 16:08:05 +0530

    Implemented the GitHub data fetching API

--- src/components/GitProfileFinder.jsx
+++ src/components/GitProfileFinder.jsx
@@ -1,72 +1,96 @@
-import { Sun, Search, MapPin, Link, Twitter, Home } from "lucide-react";
+import { Sun, Search, MapPin, Twitter, Home } from "lucide-react";
 import profileImage from "../assets/accordian.jpg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom"
 
 const GitProfileFinder = () => {
+    const [username, setUsername] = useState("");
+    const [debouncedUsername, setDebouncedUsername] = useState("");
+
+    const [userDetails, setUserDetails] = useState({});
+    // const [ repo, setRepo ] = useState("");
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedUsername(username);
+        }, 500);
+        return () => clearTimeout(timerId);
+    }, [username]);
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            const userAPi = `https://api.github.com/users/${debouncedUsername || "jha-niraj"}`;
+            // const repoAPi = `https://api.github.com/users/${debouncedUsername || "jha-niraj"}/repos`;
+            const userApiResponse = await axios.get(userAPi);
+            // const repoApiResponse = await axios.get(repoAPi);
+            setUserDetails(userApiResponse.data);
+            // setRepo(repoApiResponse.data.length);
+        }
+        fetchUserDetails();
+    }, [debouncedUsername])
+
+    console.log(userDetails);
+
     return (
-        <section className="bg-gray-900 flex flex-col gap-10 items-center justify-center h-screen text-white">
-            <div className="flex justify-between w-[50%]">
-                <h1 className="text-3xl font-bold">Github Profile Finder</h1>
-                <div className="flex items-center justify-center gap-5">
-                    <h1 className="text-xl font-semibold">Light</h1>
-                    <Sun className="fill-white" />
+        <section className=" bg-gray-800 text-white flex items-center justify-center h-screen">
+            <div className="flex items-center justify-center flex-col gap-10 w-[100%]">
+                <div className="flex justify-between w-[80%] sm:w-[70%] md:w-[60%] lg:w-[50%]">
+                    <h1 className="text-3xl font-bold">Github Profile Finder</h1>
+                    <div className="flex items-center justify-center gap-5">
+                        <h1 className="text-xl font-semibold">Light</h1>
+                        <Sun className="fill-white" />
+                    </div>
                 </div>
-            </div>
-            <div className="flex items-center justify-between shadow-2xl bg-gray-700 p-3 rounded-lg w-[50%]">
-                <div className="flex items-center justify-start gap-3 w-full">
-                    <Search className="text-blue-500" />
-                    <input type="text" placeholder="Enter your GitHub Username" className="bg-transparent pl-3 h-10 active:border-none w-[50%]" />
+                <div className="flex items-center justify-between shadow-2xl bg-gray-700 p-3 rounded-lg w-[80%] sm:w-[70%] md:w-[60%] lg:w-[50%]">
+                    <div className="flex items-center justify-start gap-3 w-full">
+                        <Search className="text-blue-500" />
+                        <input type="text" placeholder="Enter your GitHub Username" onChange={(e) => setUsername(e.target.value)} className="bg-transparent pl-3 h-10 w-[100%] placeholder:w-full" />
+                    </div>
                 </div>
-                <button className="font-bold text-md rounded-lg bg-blue-500 w-24 h-10">Search</button>
-            </div>
-            <div className="flex justify-center shadow-2xl bg-gray-700 rounded-lg p-5 w-[50%]">
-                <div className="flex items-start justify-center w-[25%]">
-                    <img className="rounded-full h-32 w-32" src={profileImage} />
-                </div>
-                <div className="flex items-center justify-center flex-col gap-5 w-[100%]">
-                    <div className="flex items-center justify-between w-full">
-                        <div>
-                            <h1 className="text-3xl font-semibold">Niraj Jha</h1>
-                            <a className="underline">@jha-niraj</a>
-                        </div>                        
-                        <div>
-                            <h1>Joined 10 00 2022</h1>
+                <div className="flex justify-center gap-5 flex-col shadow-2xl bg-gray-700 rounded-lg p-5 w-[80%] sm:w-[70%] md:w-[60%] lg:w-[50%]">
+                    <div className="flex ">
+                        <div className="flex items-start flex-col justify-around w-full">
+                            <div>
+                                <h1 className="text-3xl font-semibold">{userDetails.name}</h1>
+                                <a className="underline text-blue-500">@{userDetails.login}</a>
+                            </div>
+                            <div>
+                                <h1>{userDetails.created_at}</h1>
+                            </div>
+                        </div>
+                        <div className="flex items-start justify-center">
+                            <img className="rounded-full w-48" src={userDetails.avatar_url} />
                         </div>
                     </div>
-                    <div className="">
-                        <p className="p-4">I am Niraj Jha, currently pursuing Bachelor in Computer Science and Engineering from Lovely Professional University in my fourth semester.
-                            I'm currently working on full-stack projects like Blogging Application, Paytm clones, Text editors, etc.
-                        </p>
-                    </div>  
-                    <div className="bg-black p-5 rounded-lg w-[95%] flex items-center justify-center">
-                        <div className="w-full flex flex-col items-center justify-center">
-                            <h1 className="text-md font-semibold">Repos</h1>
-                            <p className="font-bold">51</p>
+                    <div className="flex items-center justify-center flex-col gap-5 w-[100%]">
+                        <div className="flex items-start ">
+                            <p className="">{userDetails.bio}
+                            </p>
                         </div>
-                        <div className="w-full flex flex-col items-center justify-center">
-                            <h1 className="text-md font-semibold">Followers</h1>
-                            <p className="font-bold">1</p>
+                        <div className="bg-black p-5 rounded-lg w-[100%] flex items-center justify-center">
+                            <button className="w-full flex flex-col items-center justify-center">
+                                <h1 className="text-md font-semibold">Repos</h1>
+                                <p className="font-bold">{userDetails.public_repos}</p>
+                            </button>
+                            <button className="w-full flex flex-col items-center justify-center">
+                                <h1 className="text-md font-semibold">Followers</h1>
+                                <p className="font-bold">{userDetails.followers}</p>
+                            </button>
+                            <button className="w-full flex flex-col items-center justify-center">
+                                <h1 className="text-md font-semibold">Following</h1>
+                                <p className="font-bold">{userDetails.following}</p>
+                            </button>
                         </div>
-                        <div className="w-full flex flex-col items-center justify-center">
-                            <h1 className="text-md font-semibold">Following</h1>
-                            <p className="font-bold">3</p>
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-2 justify-items-center gap-5 w-full">
-                        <div className="flex w-full items-center justify-center gap-4">
-                            <MapPin />
-                            <h1>Jalandhar, India</h1>
-                        </div>
-                        <div className="flex w-full items-center justify-center gap-4">
-                            <Link />
-                            <a>Portfolio</a>
-                        </div>
-                        <div className="flex w-full items-center justify-center gap-4">
-                            <Twitter />
-                            <a>Niraj Jha</a>
-                        </div>
-                        <div className="flex w-full items-center justify-center gap-4">
-                            <Home />
-                            <a>Siraha, Nepal</a>
+                        <div className="grid grid-cols-2 justify-items-center gap-5 w-full">
+                            <div className="flex w-full items-center justify-center gap-4">
+                                <MapPin />
+                                <h1>{userDetails.location}</h1>
+                            </div>
+                            <div className="flex w-full items-center justify-center gap-4">
+                                <Twitter />
+                                <Link to="#">{userDetails.twitter_username || "Not Available"}</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
