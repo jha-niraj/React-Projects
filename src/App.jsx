f3bbbb184940536edc81e69d37392b4ffa748617
jha-niraj <jhaniraj45@gmail.com>
2024-06-24 12:30:48 +0530

    Updated the HomePage for the Project and Updated the folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
-import MainPage from './pages/MainPage';
+
+import HomePage from './pages/HomePage';
 
 // Importing small components to render it on the Screen:
-import Accordian from "./components/Accordian/index";
-import RandomColor from './components/Random Color/randomColor';
-import GitProfileFinder from './components/GitProfileFinder';
+import Accordian from "./projects/Accordian/index";
+import RandomColor from './projects/Random Color/randomColor';
+import GitProfileFinder from './projects/GitProfileFinder';
 
 const  App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/accordian" element={<Accordian />} />
       <Route path="/randomcolor" element={<RandomColor />} />
       <Route path="/gitprofilefinder" element={<GitProfileFinder />} />
